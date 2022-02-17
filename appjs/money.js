fad969cac46a7fc0d08929db14f5d6713f014f13
atiqurrahman96 <atiqurrahmanbd302@gmail.com>
2022-02-17 22:07:51 +0600

    new file

--- appjs/money.js
+++ appjs/money.js
@@ -1,1 +1,18 @@
-console.log('i am in a separate file');
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const incomeInput = document.getElementById('income-input');
+    const totalIncome = parseFloat(incomeInput.value);
+    /* const balanceInput = document.getElementById('balance');
+    const previousBalance = parseFloat(balanceInput.innerText);
+    const mainBalance = totalIncome - totalExpense; */
+    const foodExpense = document.getElementById('food-expense').value;
+    const rentExpense = document.getElementById('rent-expense').value;
+    const clothExpense = document.getElementById('cloth-expense').value;
+    const totalExpense = parseFloat(foodExpense) + parseFloat(rentExpense) + parseFloat(clothExpense);
+    const expense = document.getElementById('total-expense');
+    const allExpense = expense.innerText;
+    expense.innerText = totalExpense;
+    const balanceInput = document.getElementById('balance');
+    const previousBalance = balanceInput.innerText;
+    const mainBalance = totalIncome - totalExpense;
+    balanceInput.innerText = mainBalance;
+})
